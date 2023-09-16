963fdb1156a3130d7164bc8bb589a7ade97ac017
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-15 23:18:10 -0300

    renomear card para cardcategoria

--- frontend/sorveteria/src/app/components/cardcategoria.js
+++ frontend/sorveteria/src/app/components/cardcategoria.js
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export default function Card({ url, categoria, link}) {
+export default function CardCategoria({ url, categoria, link}) {
     return (
         <Link href={`/cardapio/${link}`}>
             <div className="container w-56 h-80 flex flex-col justify-center items-center text-gray-400 text-xs hover:text-white" style={{backgroundImage: `url(${url})`, backgroundSize: 'cover'}}> 
--- frontend/sorveteria/src/app/page.js
+++ frontend/sorveteria/src/app/page.js
@@ -1,4 +1,4 @@
-import Card from '@/app/components/card'
+import CardCategoria from '@/app/components/cardcategoria'
 
 export default function Home() {
   return (
@@ -7,9 +7,9 @@ export default function Home() {
       <div className='flex flex-col items-center justify-center w-full'>
         <h2 className='text-3xl mb-10'>Conheça nosso cardápio</h2>
         <div className="flex flex-row justify-between w-1/2">
-          <Card categoria="Cafés" url="img_categoria-caffeteria4.jpg" link="cafes"></Card>
-          <Card categoria="Sorvetes" url="img_categoria-gelatos.jpg" link="sorvetes"></Card>
-          <Card categoria="Bolos" url="img_categoria-confeitaria.jpg" link="bolos"></Card>
+          <CardCategoria categoria="Cafés" url="img_categoria-caffeteria4.jpg" link="cafes"></CardCategoria>
+          <CardCategoria categoria="Sorvetes" url="img_categoria-gelatos.jpg" link="sorvetes"></CardCategoria>
+          <CardCategoria categoria="Bolos" url="img_categoria-confeitaria.jpg" link="bolos"></CardCategoria>
         </div>
       </div>
     </main>
