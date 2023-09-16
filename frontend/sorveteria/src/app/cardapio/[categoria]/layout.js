d53c2b9bf7f9c9912087b3dddd4b0d6910451a76
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-15 23:25:29 -0300

    criar CardapioLayout

---
+++ frontend/sorveteria/src/app/cardapio/[categoria]/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Cardapio",
+    description: "Cardapio da Sorveteria Dwitti",
+}
+
+export default function CardapioLayout({children}) {
+    return (
+        <section>
+            {children}
+        </section>
+    )
+}
