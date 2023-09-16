c2ca1d59d1ef777c9a967035de795de16ee0b23b
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-15 23:12:17 -0300

    adicionar redirecionamento nos cards

--- frontend/sorveteria/src/app/page.js
+++ frontend/sorveteria/src/app/page.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Card from '@/app/components/card'
 
 export default function Home() {
@@ -8,9 +7,9 @@ export default function Home() {
       <div className='flex flex-col items-center justify-center w-full'>
         <h2 className='text-3xl mb-10'>Conheça nosso cardápio</h2>
         <div className="flex flex-row justify-between w-1/2">
-          <Card categoria="Cafés" url="img_categoria-caffeteria4.jpg"></Card>
-          <Card categoria="Sorvetes" url="img_categoria-gelatos.jpg"></Card>
-          <Card categoria="Bolos" url="img_categoria-confeitaria.jpg"></Card>
+          <Card categoria="Cafés" url="img_categoria-caffeteria4.jpg" link="cafes"></Card>
+          <Card categoria="Sorvetes" url="img_categoria-gelatos.jpg" link="sorvetes"></Card>
+          <Card categoria="Bolos" url="img_categoria-confeitaria.jpg" link="bolos"></Card>
         </div>
       </div>
     </main>
