cdb50675ca9c8f6dab54583f95ca20a1916f9a44
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-16 11:32:47 -0300

    ajeitar estilo

--- frontend/sorveteria/src/app/components/cardproduto.js
+++ frontend/sorveteria/src/app/components/cardproduto.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function CardProduto({nome, preco, descricao, foto}) {
     return (
-        <div className="h-2/5 w-60 min-h-[210px] mx-2 rounded-md shadow-md">
+        <div className="h-2/5 w-60 min-h-[280px] mx-2 rounded-md shadow-md">
             <div className="h-4/6 relative">
             <Image 
                 src={`${foto}`}
@@ -12,8 +12,8 @@ export default function CardProduto({nome, preco, descricao, foto}) {
             />
             </div>
             <div>
-                <h1>{nome}</h1>
-                <p>{preco}</p>
+                <h1 className="text-xl font-bold">{nome}</h1>
+                <p className="font-light mb-3">R${preco}</p>
                 <p>{descricao}</p>
             </div>
         </div>
