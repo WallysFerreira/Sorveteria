6a8a7eacd8637d7938142d3c1557fd7abaa0601b
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-16 11:25:14 -0300

    mudar texto na barra de navegação

--- frontend/sorveteria/src/app/components/nav.js
+++ frontend/sorveteria/src/app/components/nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Nav() {
     return (
         <nav className='flex flex-row justify-end p-[1vh] bg-slate-500'>
-            <Link href="/login" className='border-2 px-4 bg-white'>Login</Link>
+            <Link href="/login" className='border-2 px-4 bg-white'>Entrar</Link>
         </nav>
     )
 
