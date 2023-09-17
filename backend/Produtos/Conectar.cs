cacf7cb747e1ab78704ddd0b05d1ff776bbd99a9
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-16 22:11:41 -0300

    deletar Console log inutilizado

--- backend/Produtos/Conectar.cs
+++ backend/Produtos/Conectar.cs
@@ -6,7 +6,6 @@ namespace Produtos;
 
 public class Conectar {
     public static async void Inserir(Produto prod) {
-        Console.WriteLine("Fui chamado");
 
         try {
             var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
