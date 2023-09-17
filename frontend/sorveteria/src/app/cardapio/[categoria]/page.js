33316d963ba88862c9d91a35609a8346c95c832a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-16 22:56:32 -0300

    pegar produtos da api

--- frontend/sorveteria/src/app/cardapio/[categoria]/page.js
+++ frontend/sorveteria/src/app/cardapio/[categoria]/page.js
@@ -1,8 +1,40 @@
 import CardProduto from "@/app/components/cardproduto"
 
-export default function Page() {
+export async function getData() {
+    const produtos = await fetch('http://localhost:5172/api/produtos').then((res) => res.json())
+
+    return produtos
+}
+export async function generateStaticParams() {
+    const produtos = await getData()
+
+    return produtos.map((produto) => ({
+        categoria: produto.categoria,
+    }))
+}
+
+export default async function Page({ params }) {
+    const produtos = await getData()
+
     return (
         <section className="p-2 flex flex-wrap h-full w-full">
+        {produtos.map((produto) => { 
+            if (params.categoria == produto.categoria.toLowerCase()) {
+            return (
+                <CardProduto
+                nome={produto.nome}
+                descricao={produto.descricao}
+                preco={produto.preco}
+                foto={produto.foto}
+            />
+            )
+            }
+                
+        })}
+        </section>
+
+    )
+            /*
             <CardProduto 
                 nome="Sorvete normal"
                 descricao="É um sorvete normal"
@@ -51,6 +83,6 @@ export default function Page() {
                 preco="12.00"
                 foto="/teste.jpg"
             />
-        </section>
     )
+            */
 }
