daa822151e6d3218a2de932c5ef101fc5333e4d6
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-26 12:30:06 -0300

    criar formulario de cadastro de conta

---
+++ frontend/sorveteria/src/app/(contas)/cadastro/page.js
@@ -0,0 +1,29 @@
+export default function Page() {
+    return (
+        <form action={null} className="h-3/5 w-2/5 px-48 py-32 border-2 rounded-lg shadow-md flex flex-col justify-around items-center">
+            <div className="flex flex-row">
+                <div className="flex flex-col">
+                    <label htmlFor="nome">Nome</label>
+                    <input className="border-2" type="text" name="nome" id="nome" required />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="sobrenome">Sobrenome</label>
+                    <input className="border-2" type="text" name="sobrenome" id="sobrenome" required />
+                </div>
+            </div>
+            
+            <div className="flex flex-col">
+                <label htmlFor="email">Email</label>
+                <input className="border-2" type="email" name="email" id="email" required />
+            </div>
+
+            <div className="flex flex-col">
+                <label htmlFor="senha">Senha</label>
+                <input className="border-2" type="senha" name="senha" id="senha" required />
+            </div>
+
+            <button className="border-2" type="submit">Cadastrar</button>
+        </form>
+    )
+}
