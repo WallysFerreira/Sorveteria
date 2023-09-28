de179bea2a9745123c3a10e0c395f9103d2a4ef4
Wallys Ferreira <wallysfsilva@gmail.com>
2023-09-27 21:19:24 -0300

    pegar produto usando id passado como parametro no url

--- frontend/sorveteria/src/app/(adm)/produtos/editar/[id]/page.js
+++ frontend/sorveteria/src/app/(adm)/produtos/editar/[id]/page.js
@@ -5,14 +5,13 @@ export async function generateStaticParams() {
     const produtos = await getListaProdutos();
 
     return produtos.map((prod) => ({
-        id: prod.Id,
-        produto: prod,
+        id: prod.id.toString(),
     }))
 }
 
 // Mostrar um formulario para editar um produto especifico
 export default async function Page({ params }) {
-    const produto = params.produto
+    const produto = await fetch(`http://localhost:5172/api/produtos/${params.id}`).then((res) => res.json())
 
     return (
         <FormAtualizarProduto produto={produto} />
