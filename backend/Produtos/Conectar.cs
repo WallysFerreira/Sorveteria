ba7b28cb3c0ae180f2a49aaf167ea49476ff0061
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-01 00:16:57 -0300

    usar variaveis env

--- backend/Produtos/Conectar.cs
+++ backend/Produtos/Conectar.cs
@@ -1,14 +1,15 @@
-using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc.Routing;
 using MySqlConnector;
 using Produtos.Models;
 
 namespace Produtos;
 
 public class Conectar {
-    public static async Task<Produto?> Inserir(Produto prod) {
+    static string url = Environment.GetEnvironmentVariable("DB_URL");
 
+    public static async Task<Produto?> Inserir(Produto prod) {
         try {
-            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
+            var connection = new MySqlConnection("Server="+url+";User ID=root;Password=root;Database=Produtos");
             await connection.OpenAsync();
 
             var queryInsert = new MySqlCommand("INSERT INTO Produtos (Categoria, Nome, Preco, Descricao, Foto) VALUES (@c, @n, @p, @d, @f)", connection);
@@ -44,7 +45,7 @@ public class Conectar {
     public static async Task<Produto?> PegarUm(int id) {
         Produto? prod = null;
         try {
-            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
+            var connection = new MySqlConnection("Server="+url+";User ID=root;Password=root;Database=Produtos");
             await connection.OpenAsync();
 
             var querySelectUm = new MySqlCommand("SELECT * FROM Produtos WHERE ID = (@p)", connection);
@@ -69,7 +70,7 @@ public class Conectar {
         List<Produto> produtos = new();
 
         try {
-            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
+            var connection = new MySqlConnection("Server="+url+";User ID=root;Password=root;Database=Produtos");
             await connection.OpenAsync();
 
             var querySelect = new MySqlCommand("SELECT * FROM Produtos", connection);
@@ -93,7 +94,7 @@ public class Conectar {
         Produto? prod = null; 
 
         try {
-            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
+            var connection = new MySqlConnection("Server="+url+";User ID=root;Password=root;Database=Produtos");
             await connection.OpenAsync();
 
             var querySelectUm = new MySqlCommand("SELECT * FROM Produtos WHERE ID = (@p)", connection);
@@ -130,7 +131,7 @@ public class Conectar {
         Produto? prod = null; 
 
         try {
-            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
+            var connection = new MySqlConnection("Server="+url+";User ID=root;Password=root;Database=Produtos");
             await connection.OpenAsync();
 
             var querySelectUm = new MySqlCommand("SELECT * FROM Produtos WHERE ID = (@p)", connection);
@@ -178,7 +179,7 @@ public class Conectar {
         Produto? prod = null; 
 
         try {
-            var connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=Produtos");
+            var connection = new MySqlConnection("Server="+url+";User ID=root;Password=root;Database=Produtos");
             await connection.OpenAsync();
 
             var querySelectUm = new MySqlCommand("SELECT * FROM Produtos WHERE ID = (@p)", connection);
