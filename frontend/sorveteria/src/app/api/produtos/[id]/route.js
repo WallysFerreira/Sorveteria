2cdeeac34de9906f35035fcdb8e5e584ff85097d
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-04 02:30:34 +0000

    adicionar put na rota especifica

--- frontend/sorveteria/src/app/api/produtos/[id]/route.js
+++ frontend/sorveteria/src/app/api/produtos/[id]/route.js
@@ -16,3 +16,29 @@ export async function DELETE(req, { params }) {
         status: 200
     })
 }
+
+export async function PUT(req, { params }) {
+    const formData = await req.formData()
+
+    const categoria = formData.get('categoria')
+    const nome = formData.get('nome')
+    const preco = parseFloat(formData.get('preco'))
+    const descricao = formData.get('descricao')
+    const foto = formData.get('foto')
+
+    const json = {
+        'categoria': categoria,
+        'nome': nome,
+        'preco': preco,
+        'descricao': descricao,
+        'foto': foto
+    }
+
+    const res = await fetch(`http://produtos-service:80/api/produtos${params.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(json)
+    })
+}
